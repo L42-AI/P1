2599b4c6fe45bb600cc4241bb4a440d56379ec1d
L42-AI <lzaitch@gmail.com>
2025-11-10 23:46:12 +0100

    migrate to class

--- solver.py
+++ solver.py
@@ -7,96 +7,82 @@ Implement: solve_cnf(clauses) -> (status, model_or_None)"""
 
 
 from typing import Iterable, List, Tuple
-import random as r
+from collections import deque
 
+Clause = Iterable[int]
+Clauses = Iterable[Clause]
 
+def flip(value: int) -> int:
+    return -1 if value == 1 else -1
 
-def solve_cnf(clauses: Iterable[Iterable[int]], num_vars: int) -> Tuple[str, List[int] | None]:
-    """
-    Implement your SAT solver here.
-    Must return:
-      ("SAT", model)  where model is a list of ints (DIMACS-style), or
-      ("UNSAT", None)
-    """ 
+class DPLL:
+    def __init__(self, clauses: Clauses, num_vars: int):
+        self.clauses = clauses
+        
+        self.assignment = {var: 0 for var in range(1, num_vars + 1)} # 0: None 1: True -1: False
+        self.assignment_trail = deque()
 
-    def element_true(element, assignment):
-        return (element < 0 and assignment[abs(element)] == -1) or (element > 0 and assignment[abs(element)] == 1)
+    def _assign(self, variable: int, value: int):
+        self.assignment[abs(variable)] = value
+        self.assignment_trail.append(variable)
 
-    def get_pending_variables(clause, assignment) -> set[int]:
-        pending_variables = set()
+    def element_true(self, variable: int):
+        return (variable < 0 and self.assignment[abs(variable)] == -1) or (variable > 0 and self.assignment[abs(variable)] == 1)
+
+    def clause_is_true(self, clause: Clause):
         for element in clause:
-            if assignment[abs(element)] == 0:
-                pending_variables.add(element)
-
-        return pending_variables
-    
-    def clause_is_true(clause, assignment):
-        for element in clause:
-            if element_true(element, assignment):
+            if self.element_true(element):
                 return True
             
         return False
 
-    def propogate(clauses, assignment):
-        
+    def get_pending_variables(self, clause: Clause) -> set[int]:
+        pending_variables = set()
+        for element in clause:
+            if self.assignment[abs(element)] == 0:
+                pending_variables.add(element)
+
+        return pending_variables
+    
+
+    def propagate(self):
         must_propogate = False
-        for clause in clauses:
-            if clause_is_true(clause, assignment):
+        for clause in self.clauses:
+            if self.clause_is_true(clause):
                 continue
             
-            pending_variables = get_pending_variables(clause, assignment)
+            pending_variables = self.get_pending_variables(clause)
 
             if len(pending_variables) == 1:
                 v = list(pending_variables)[0]
-                assignment[abs(v)] = -1 if v < 0 else 1
+                self._assign(v, -1 if v < 0 else 1)
                 must_propogate = True
                 
         if not must_propogate:
-            return clauses, assignment
+            return 
         
-        propogate(clauses, assignment)
+        self.propagate()
 
-    def get_variable(clauses, assignment): # TO BE IMPROVED WITH HEURISTIC
-        for clause in clauses:
-            for element in clause:
-                if assignment[abs(element)] == 0:
-                  return element
-
-    def solved(clauses, assignment):
-        for clause in clauses:
-            if not clause_is_true(clause, assignment):
+    def is_solved(self):
+        for clause in self.clauses:
+            if not self.clause_is_true(clause):
                 return False
         return True
 
-    assignment = {var: 0 for var in range(1, num_vars + 1)} # 0: None 1: True -1: False
-
-    choices_made = {}
-
-    is_solved = False
-    while not is_solved:
-
-        v = get_variable(clauses, assignment)
-        if v is None:
-            for v, choice in choices_made.items():
-                assignment[v] = flip(choice)
-                choices_made[v] = flip(choice)
-        else:
-            choice = 1 if r.random() < 0.5 else -1
-            assignment[abs(v)] = choice
-            choices_made[abs(v)] = choice
-
-
+    def solve(self):
+        is_solved = False
         must_propogate = True
-      
-        if must_propogate:
-            propogate(clauses, assignment)
-            must_propogate = False
-        
-        if solved(clauses, assignment):
-            is_solved = True
-        
-    return 'SAT', [v for v in assignment.values() if v == 1]
-    return 'UNSAT', None
+        while not is_solved:
 
-def flip(value: int) -> int:
-    return -1 if value == 1 else -1
+            if must_propogate:
+                self.propagate()
+                must_propogate = False
+
+            if self.is_solved():
+                is_solved = True
+            
+        return 'SAT', [v for v in self.assignment.values() if v == 1]
+        return 'UNSAT', None
+    
+def solve_cnf(clauses: Iterable[Iterable[int]], num_vars: int) -> Tuple[str, List[int] | None]:
+    return DPLL(clauses, num_vars).solve()
